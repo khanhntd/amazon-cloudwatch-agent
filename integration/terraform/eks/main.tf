4dc3eb52697fb0123fb57458e697d3cc24ed3e8d
Khanh Nguyen <khanhntd@amazon.com>
2022-02-03 11:04:56 -0500

    add kube config

--- integration/terraform/eks/main.tf
+++ integration/terraform/eks/main.tf
@@ -41,3 +41,37 @@ resource "kubernetes_namespace" "aoc_ns" {
     name = "aoc-ns-${module.common.testing_id}"
   }
 }
+
+resource "kubernetes_service_account" "aoc-role" {
+  metadata {
+    name      = "aoc-role-${module.common.testing_id}"
+    namespace = kubernetes_namespace.aoc_ns.metadata[0].name
+  }
+
+  automount_service_account_token = true
+}
+
+resource "kubernetes_service_account" "aoc-agent-role" {
+  count = 1
+  metadata {
+    name      = "aoc-agent-${module.common.testing_id}"
+    namespace = var.deployment_type == "fargate" ? tolist(aws_eks_fargate_profile.test_profile[count.index].selector)[0].namespace : kubernetes_namespace.aoc_ns.metadata[0].name
+  }
+
+  automount_service_account_token = true
+  depends_on                      = [aws_eks_fargate_profile.test_profile]
+}
+
+data "template_file" "kubeconfig_file" {
+  template = file("./kubeconfig.tpl")
+  vars = {
+    CA_DATA : data.aws_eks_cluster.testing_cluster.certificate_authority[0].data
+    SERVER_ENDPOINT : data.aws_eks_cluster.testing_cluster.endpoint
+    TOKEN = data.aws_eks_cluster_auth.testing_cluster.token
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  filename = "kubeconfig"
+  content  = data.template_file.kubeconfig_file.rendered
+}
